a2aa80231a7950cc3a62aa3a0bb11b3e26577a65
kencuo <2576079939@qq.com>
2026-01-27 23:24:50 +0800

    Update three-vrm.module.js

--- vendor/three-vrm.module.js
+++ vendor/three-vrm.module.js
@@ -6,8 +6,8 @@
  * @pixiv/three-vrm is distributed under MIT License
  * https://github.com/pixiv/three-vrm/blob/release/LICENSE
  */
-import * as THREE from './three.module.js?v=2026012705';
-import { BufferAttribute } from './three.module.js?v=2026012705';
+import * as THREE from './three.module.js?v=2026012706';
+import { BufferAttribute } from './three.module.js?v=2026012706';
 
 /*!
  * @pixiv/three-vrm-core v2.0.0
