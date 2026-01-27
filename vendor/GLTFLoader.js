626270e5dd16046c8a16fbf201f35c607307faf6
kencuo <2576079939@qq.com>
2026-01-27 21:46:39 +0800

    Update GLTFLoader.js

--- vendor/GLTFLoader.js
+++ vendor/GLTFLoader.js
@@ -64,7 +64,7 @@ import {
 	VectorKeyframeTrack,
 	SRGBColorSpace,
 	InstancedBufferAttribute
-} from './three.module.js';
+} from './three.module.js?v=20260127';
 import { toTrianglesDrawMode } from '../utils/BufferGeometryUtils.js';
 
 class GLTFLoader extends Loader {
